3fb86cb559b0a2858f83c555318f838bc62bc67b
Dylan Alcock <dylan.alcock@gmail.com>
2020-04-08 14:00:08 -0600

    Add friend page image and markdownx copyright

--- web_server/posts/templatetags/show_markdown.py
+++ web_server/posts/templatetags/show_markdown.py
@@ -1,3 +1,9 @@
+"""
+MarkdownX is licensed under the 2-clause BSD license, and Open Source Initiative approved license.
+Copyright (c) 2016, Adrian Drzewicki (adriandrzewicki@gmail.com)
+All rights reserved.
+"""
+
 from django import template
 from markdownx.utils import markdownify
 
